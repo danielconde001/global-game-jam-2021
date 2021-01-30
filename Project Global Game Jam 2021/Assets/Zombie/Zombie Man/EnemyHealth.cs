e8f8116fc0a47a0eaf6a1f4545a6bda593ebd9a8
danielconde001 <danielconde001@yahoo.com>
2021-01-30 14:00:14 +0800

    no message

---
+++ Project Global Game Jam 2021/Assets/Zombie/Zombie Man/EnemyHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : EntityHealth
+{
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Death()
+    {
+        base.Death();
+    }
+}
