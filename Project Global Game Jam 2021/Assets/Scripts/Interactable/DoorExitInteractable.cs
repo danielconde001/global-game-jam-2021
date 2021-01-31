41d2c36931482076d4f8ac4e21f1e0e76181acb7
danielconde001 <danielconde001@yahoo.com>
2021-01-31 15:14:51 +0800

    yolo

--- Project Global Game Jam 2021/Assets/Scripts/Interactable/DoorExitInteractable.cs
+++ Project Global Game Jam 2021/Assets/Scripts/Interactable/DoorExitInteractable.cs
@@ -1,15 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DoorExitInteractable : Interactable
 {
     [SerializeField] protected Room room;
 
+    public UnityEvent OnInteractLocked;
+    public UnityEvent OnInteractUnlocked;
+
+    [SerializeField] protected bool unlocked = true;
+
     public override void Interact()
     {
         base.Interact();
+        
+        if (unlocked) room.HasFinishedRoom();
+        if (!unlocked) OnInteractLocked.Invoke();
 
-        room.HasFinishedRoom();
+    }
+
+    public void ShowText(string interactText)
+    {
+        PlayerInteractContext.current.ShowText(interactText);
+    }
+
+    public void Unlock()
+    {
+        unlocked = true;
     }
 }
